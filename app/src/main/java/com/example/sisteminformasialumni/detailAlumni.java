1fb87668e04fede7b468870e2034548eb9f1426a
rezalfara <reza.alfara07@gmail.com>
2023-11-07 02:05:47 +0700

    commit

--- app/src/main/java/com/example/sisteminformasialumni/detailAlumni.java
+++ app/src/main/java/com/example/sisteminformasialumni/detailAlumni.java
@@ -5,9 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
